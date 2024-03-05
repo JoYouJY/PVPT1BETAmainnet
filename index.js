3aafe6072fd653534693d0658a3827b215a9d9f6
Jojo <neeya.blog@gmail.com>
2024-03-05 09:38:25 +0800

    move rotate and switchChain to after loading

--- index.js
+++ index.js
@@ -495,8 +495,8 @@ async function switchToFantom() {
 
 // Call the connectToFantom function to connect to the Fantom chain
 //connectToFantom();
-setTimeout(switchToFantom, 1000);
-/*
+setTimeout(switchToFantom, 4127);
+
 // Get a reference to the button element
 const rotateButton = document.getElementById("unity-rotate-button");
 
@@ -519,4 +519,3 @@ function rotateCanvas() {
   
   response(response_type.ROTATE, isHorizontal);
 }
-*/
